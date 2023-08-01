127e6ca04132424dee6e818dda1e39ecded4e5d3
avblackwell <alexvblackwell@gmail.com>
2023-08-01 10:37:28 -0400

    move images from src to public

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
       <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <a href="#" class="-m-1.5 p-1.5">
           <span class="sr-only">Your Company</span>
-          <img class="h-16 w-auto" src="src/images/logo.svg" alt="" />
+          <img class="h-16 w-auto" src="/images/logo.svg" alt="" />
         </a>
         <div class="flex lg:hidden">
           <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
@@ -22,7 +22,7 @@
           <div class="flex items-center justify-between">
             <a href="#" class="-m-1.5 p-1.5">
               <span class="sr-only">Your Company</span>
-              <img class="h-16 w-auto" src="src/images/logo.svg" alt="" />
+              <img class="h-16 w-auto" src="/images/logo.svg" alt="" />
             </a>
             <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
               <span class="sr-only">Close menu</span>
