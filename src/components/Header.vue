0e8fe11d5348a094aca7460ead76f5f0c553c380
avblackwell <alexvblackwell@gmail.com>
2023-08-01 14:17:21 -0400

    update for meeting

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,7 +3,7 @@
       <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <a href="#" class="-m-1.5 p-1.5">
           <span class="sr-only">Your Company</span>
-          <img class="h-16 w-auto" src="/images/logo.svg" alt="" />
+          <img class="h-20 w-auto" src="/images/logo-2.png" alt="" />
         </a>
         <div class="flex lg:hidden">
           <button type="button" class="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = true">
@@ -11,9 +11,9 @@
             <Bars3Icon class="h-6 w-6" aria-hidden="true" />
           </button>
         </div>
-        <div class="hidden lg:flex lg:gap-x-12">
-          <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-sm font-semibold leading-6 text-gray-900">{{ item.name }}</a>
-          <a href="#" class="text-sm font-semibold leading-6 text-gray-900">Give <span aria-hidden="true">&rarr;</span></a>
+        <div class="hidden lg:flex lg:gap-x-7">
+          <a v-for="item in navigation" :key="item.name" :href="item.href" class="text-lg font-semibold leading-6 text-gray-900">{{ item.name }}</a>
+          <a href="#" class="text-lg font-semibold leading-6 text-gray-900">Give <span aria-hidden="true">&rarr;</span></a>
         </div>
       </nav>
       <Dialog as="div" class="lg:hidden" @close="mobileMenuOpen = false" :open="mobileMenuOpen">
@@ -22,7 +22,7 @@
           <div class="flex items-center justify-between">
             <a href="#" class="-m-1.5 p-1.5">
               <span class="sr-only">Your Company</span>
-              <img class="h-16 w-auto" src="/images/logo.svg" alt="" />
+              <img class="h-20 w-auto" src="/images/logo-2.png" alt="" />
             </a>
             <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
               <span class="sr-only">Close menu</span>
